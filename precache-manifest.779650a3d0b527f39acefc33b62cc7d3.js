02e2dec229cd070d9b0400ce0d7911a01dd46df2
harry <harry@earlytrade.com>
2020-04-25 23:19:07 +1000

    Updates

---
+++ precache-manifest.779650a3d0b527f39acefc33b62cc7d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f89d0214bffc2a6f3f9d325a4c3f5d0e",
+    "url": "/website_redux/index.html"
+  },
+  {
+    "revision": "bc6d0b7d2aa2f7d017d0",
+    "url": "/website_redux/static/css/main.a406f0b3.chunk.css"
+  },
+  {
+    "revision": "1967fa8d25934e1df548",
+    "url": "/website_redux/static/js/2.573d33ca.chunk.js"
+  },
+  {
+    "revision": "13425e2e45fee351b4139959073d7e4f",
+    "url": "/website_redux/static/js/2.573d33ca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc6d0b7d2aa2f7d017d0",
+    "url": "/website_redux/static/js/main.81af7680.chunk.js"
+  },
+  {
+    "revision": "a0fbfc1e53058e3497cc",
+    "url": "/website_redux/static/js/runtime-main.450873da.js"
+  },
+  {
+    "revision": "d6c8bd35e1a53baa977a64883ca1c11d",
+    "url": "/website_redux/static/media/ring.d6c8bd35.png"
+  }
+]);
